9ebe4aaf02768fa644cfc32c0f2d51c7fa6104b3
Jacky Chiu <jacky.chiu888@gmail.com>
2015-02-21 14:49:52 -0500

    Test for new files

---
+++ De/De/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  De
+//
+//  Created by Jacky Chiu on 2015-02-21.
+//  Copyright (c) 2015 Jacky Chiu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
