f904e31e72d38171126c56942a0e4d7b0858c225
Jacky Chiu <jacky.chiu888@gmail.com>
2015-02-21 22:08:05 -0500

    FIxing

--- assignment1/2DArrayQ1.cpp
+++ assignment1/2DArrayQ1.cpp
@@ -1,6 +1,3 @@
-//////////
-//new
-/////////
 #include <iostream>
 
 using namespace std;
@@ -11,24 +8,32 @@ void rprintarray(int [][5],int,int);
 void diagonal(int [][5]);
 void sum(int [][5]);
 void rsum(int [][5],int,int,int);
-//void sumrow(int [][5],int,int);
+void sumrow(int [][5]);
+void largestrow(int [][5],int);
 
 int main() {
     
     int a[5][5];
     int p1=0,p2=0,s=0;
+    
+    
     random(a);
-    cout<<endl;
+    
     printarray(a);
-     cout<<endl;
+    
     rprintarray(a,p1,p2);
-    cout<<endl;
+    
     diagonal(a);
-    cout<<endl;
+    
     sum(a);
-    cout<<endl;
+    
     rsum(a,p1,p2,s);
-       return 0;
+    
+    sumrow(a);
+    
+    largestrow(a,p1);
+    
+    return 0;
 }
 
 void random(int a[][5])
@@ -63,18 +68,18 @@ void rprintarray(int a[][5],int p1,int p2)
         cout<<endl;
         return;
     }
+    else
+    {
+        if(p2==4)
+        {
+            cout<<endl;
+            rprintarray(a,p1+1,0);
+        }
         else
         {
-            if(p2==4)
-            {
-                cout<<endl;
-                rprintarray(a,p1+1,0);
-            }
-            else
-            {
-                rprintarray(a,p1,p2+1);
-            }
+            rprintarray(a,p1,p2+1);
         }
+    }
 }
 
 void diagonal(int a[][5])
@@ -110,7 +115,7 @@ void sum(int a[][5])
         }
     }
     cout<<"Sum: "<<sum<<endl;
-
+    
 }
 
 void rsum(int a[][5],int p1,int p2,int s)
@@ -122,15 +127,52 @@ void rsum(int a[][5],int p1,int p2,int s)
         cout<<"Sum: "<<s<<endl;
         return;
     }
+    else
+    {
+        if(p2==4)
+        {
+            rsum(a,p1+1,0,s);
+        }
         else
         {
-            if(p2==4)
-            {
-                rsum(a,p1+1,0,s);
-            }
-            else
-            {
-                rsum(a,p1,p2+1,s);
-            }
+            rsum(a,p1,p2+1,s);
         }
+    }
+}
+
+void sumrow(int a[][5])
+{
+    int sum=0;
+    
+    for(int x=0;x<=4;x++)
+    {
+        sum=0;
+        
+        for(int y=0;y<=4;y++)
+        {
+            sum=sum+a[x][y];
+        }
+        
+        cout<<"Sum row "<<x+1<<" :"<<sum<<endl;
+    }
+}
+
+void largestrow(int a[][5],int p1)
+{
+    int sum=0;
+    
+    for(int x=0;x<=4;x++)
+    {
+        sum=0;
+        
+        for(int y=0;y<=4;y++)
+        {
+            sum=sum+a[x][y];
+        }
+        if(sum>p1)
+        {
+            p1=sum;
+        }
+    }
+    cout<<"Largest row sum: "<<p1<<endl;
 }
