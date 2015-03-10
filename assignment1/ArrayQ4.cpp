083e33621fd3fd1d5a31e329c33b711621dd3f6a
Jacky Chiu <jacky.chiu888@gmail.com>
2015-03-09 22:36:51 -0400

    Updated file

--- assignment1/ArrayQ4.cpp
+++ assignment1/ArrayQ4.cpp
@@ -2,111 +2,34 @@
 
 using namespace std;
 
-void init_array(int []);
-void printarray(int []);
-void printarrayrecursive(int [],int);
-int sum(int []);
-void rsum(int [],int,int);
-void largest(int [],int);
-void rlargest(int [],int,int);
-
 int main() {
     
-    int a[10]={0};
-    int x=9;
-    int y=0;
-    srand(time(0));
+    int q,sum=0;
+    char a[10000];
+    char ca[10000];
     
-    init_array(a);
-    printarray(a);
-    printarrayrecursive(a,x);
-    cout<<sum(a)<<endl;
-    rsum(a,x,y);
-    largest(a,y);
-    rlargest(a,x,y);
     
-    return 0;
-}
-
-void init_array(int a[])
-{
-    for(int x=0;x<=9;x++)
-    {
-        a[x]=rand()%50+1;
-    }
-}
-
-void printarray(int a[])
-{
-    for(int x=0;x<=9;x++)
-    {
-        cout<<a[x]<<endl;
-    }
-}
-
-void printarrayrecursive(int a[],int x)
-{
-    cout<<a[x]<<endl;
-    if(x==0)
-    {
-        return ;
-    }
-    else
-    {
-        printarrayrecursive(a,x-1);
-    }
-}
-
-int sum(int a[])
-{
-    int sum=0;
-    for(int x=0;x<=9;x++)
-    {
-        sum=sum+a[x];
-    }
-    return sum;
+    cin>>q;
     
-}
-
-void rsum(int a[],int x,int y)
-{
-    y=y+a[x];
-    if(x==0)
+    for(int x=0;x<q;x++)
     {
-        cout<<y<<endl;
-    }
-    else
-    {
-        rsum(a,x-1,y);
+        cin>>a[x];
     }
     
-}
-
-void largest(int a[],int y)
-{
-    for(int x=0;x<=9;x++)
+    for(int y=0;y<q;y++)
     {
-        if(a[x]>y)
+        cin>>ca[y];
+    }
+    
+    for(int i=0;i<q;i++)
+    {
+        if(a[i]==ca[i])
         {
-            y=a[x];
+            sum++;
         }
     }
-    cout<<y<<endl;
-}
-
-void rlargest(int a[],int x,int y)
-{
-    if(a[x]>y)
-    {
-        y=a[x];
-    }
     
-    if(x==0)
-    {
-        cout<<y<<endl;
-    }
-    else
-    {
-        rlargest(a,x-1,y);
-    }
+    cout<<sum<<endl;
+    
+    return 0;
 }
