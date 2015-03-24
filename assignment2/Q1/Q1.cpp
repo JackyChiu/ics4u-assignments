ba064d3c2e3294bdcf87df31f2e5f98bde1d0c8a
Jacky Chiu <jacky.chiu888@gmail.com>
2015-03-24 18:35:34 -0400

    Changed with a improved algorithm of the code

--- assignment2/Q1/Q1.cpp
+++ assignment2/Q1/Q1.cpp
@@ -1,56 +1,38 @@
 #include <iostream>
-#include<fstream>
 
 using namespace std;
 
-void pattern(int,int,int[]);
-int factorial(int x);
+int factorial(int);
 int total_comb(int,int);
-bool lastcomb(int,int,int []);
-void move(int,int,int [],int);
-void poolmove(int,int,int[],int);
-void pattern(int,int,int []);
+void all_combination(int,int,int,int[],int[]);
+void next_combination(int,int,int,int[],int[]);
 
-ifstream inPat( "/Users/i2Chinese/Documents/School/ICS4U/Practice/Practice/pat.in");
-ofstream outPat( "/Users/i2Chinese/Documents/School/ICS4U/Practice/Practice/pat.out");
 
 int main() {
     
-    int pair,n,k;
-    int *set;
+    int *acombination;
+    int *binary;
+    int n,r,check;
     
-    if (! inPat){
-        cerr<<"File could not be open\n";
-        exit(1);}
+    cin>>n;
+    cin>>r;
     
-    inPat>>pair;
+    binary= new int [n];
+    acombination= new int [r];
+    check=r;
     
-    for(int a=0;a<pair;a++)
+    for(int a=0;a<n;a++)
     {
-        outPat<<"The bit patterns are: "<<endl;
-        
-        inPat>>n;
-        inPat>>k;
-        
-        set=new int [n];
-        
-        for(int a=0;a<n;a++)
-        {
-            if(a<k)
-            {
-                set[a]=1;
-            }
-            else
-            {
-                set[a]=0;
-            }
-            cout<<set[a];
-        }
-        
-        pattern(n,k,set);
-        
-        outPat<<endl;
+        binary[a]=0;
     }
+    
+    for(int a=1;a<=r;a++)
+    {
+        acombination[a]=a;
+    }
+    
+    all_combination(n,r,check,acombination,binary);
+    
     return 0;
 }
 
@@ -72,107 +54,61 @@ int total_comb(int n,int r)
     return total_comb;
 }
 
-bool lastcomb(int n,int k,int set[])
+void all_combination(int n,int r,int check,int acombination[],int binary[])
 {
-    int finalcomb=0; //finds final combination
-    
-    for(int a=n-k;a<n;a++)
+    for(int a=0;a<r;a++)
     {
-        finalcomb=finalcomb+set[a];
-    }
-    
-    if(finalcomb==k)
-    {
-        return true; //ends
-    }
-    
-    else
-    {
-        return false;
-    }
-    
-}
-
-void move(int n,int k,int set[],int i)
-{
-    int last; //last position
-    
-    for(int a=n-1;a>=0;a--)
-    {
-        if(set[a]==1)
-        {
-            last=a;
-            break;
-        }
-    }
-    
-    for(int a=last;a<n;a++)
-    {
-        set[last+1]=1;
-        set[last]=0;
-    }
-}
-
-void poolmove(int n,int k,int set[],int magnitude)
-{
-    
-    for(int a=0;a<n;a++)
-    {
-        if(a<k)
-        {
-            set[a]=1;
-        }
-        else
-        {
-            set[a]=0;
-        }
-    }
-    
-    
-    for(int a=magnitude;a>=0;magnitude--)
-    {
-        if(set[a]==1)
-        {
-            set[a+1]=1;
-            set[a]=0;
-        }
+        binary[acombination[a]]=1;
     }
     
     for(int a=0;a<n;a++)
     {
-        cout<<set[a];
+        cout<<binary[a];
+    }
+    cout<<endl;
+    
+    for(int b=1;b<=total_comb(n,r);b++)
+    {
+        next_combination(n,r,check,acombination,binary);
     }
     
+}
+
+void next_combination(int n,int r,int check,int acombination[],int binary[])
+{
+    for(int a=0;a<n;a++)
+    {
+        binary[a]=0;
+    }
+    
+    while(acombination[check]==(n-r+check))
+    {
+        check--;
+    }
+    acombination[check]++;
+    
+    for(int a=check;a<=r;a++)
+    {
+        acombination[a]=acombination[check]+a-check;
+    }
+    
+    for(int a=0;a<r;a++)
+    {
+        binary[acombination[a]]=1;
+    }
+    
+    for(int a=1;a<=r-1;a++)
+    {
+        cout<<acombination[a]<<", ";
+    }
+    cout<<acombination[r]<<endl;
+    
+    for(int a=0;a<n;a++)
+    {
+        cout<<binary[a];
+    }
     cout<<endl;
 }
 
-void pattern(int n,int k,int set[])
-{
-    int i=(k-1);
-    int newi=i;
-    int magnitude=(k-1);
-    
-    while(lastcomb(n,k,set)==false)
-    {
-        while(i<(n-1)&&i>=0)
-        {
-            cout<<endl;
-            
-            if((set[i]==1)&&i<n)
-            {
-                move(n,k,set,i);
-            }
-            
-            i++;
-            
-            for(int a=0;a<n;a++)
-            {
-                cout<<set[a];
-            }
-        }
-        poolmove(n,k,set,magnitude);
-        magnitude++;
-        newi++;
-        i=newi;
-    }
-}
+
+
