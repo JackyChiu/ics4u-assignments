315deb34b88191bbc110963cefca7723a43b0190
Jacky Chiu <jacky.chiu888@gmail.com>
2015-04-30 22:30:19 -0400

    Added ostream friend funtions to the classes

--- assignment5/Q1.cpp
+++ assignment5/Q1.cpp
@@ -4,9 +4,12 @@ using namespace std;
 
 class Point
 {
+    friend ostream &operator<<(ostream&,Point&);
 public:
     Point(int=0,int=0);
     void setvalue(int,int);
+    int gety() {return x;};
+    int getx();
 protected:
     int x;
     int y;
@@ -24,8 +27,15 @@ void Point::setvalue(int a,int b)
     y=b;
 }
 
+ostream &operator<<(ostream& cout,Point &b)
+{
+    cout<<b.x<<","<<b.y;
+    return cout;
+}
+
 class Square:public Point
 {
+    friend ostream &operator<<(ostream&,Square&);
 public:
     Square(int=1,int=0,int=0);
     void setvalue(int);
@@ -50,8 +60,15 @@ int Square::area()
     return (base*base);
 }
 
+ostream &operator<<(ostream&cout,Square&b)
+{
+    cout<<b.base;
+    return cout;
+}
+
 class Cube: public Square
 {
+    friend ostream &operator<<(ostream&,Cube&);
 public:
     Cube(int=1,int=0,int=0);
     void setvalue(int);
@@ -83,5 +100,16 @@ int Cube::volume()
     return (length*Square::area());
 }
 
+ostream &operator<<(ostream&cout,Cube&b)
+{
+    cout<<b.length;
+    return cout;
+}
 
-
+int main()
+{
+    Cube(5,7,8);
+    
+    
+    return 0;
+}
